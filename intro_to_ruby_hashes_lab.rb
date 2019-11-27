6bdd76522fc0355f0ecbe81eb09c0668f0577338
James Inglis <jinglis12@googlemail.com>
2019-11-27 15:03:53 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -33,5 +33,8 @@ def read_from_hash(hash, key)
 end
 
 def update_counting_hash(hash, key)
-  hash.fetch(key) { | key | hash[key] += 1 }
+  if hash[key]
+    hash[key] += 1
+  else hash[key] = 1
+  end
 end
